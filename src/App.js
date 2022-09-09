b2bbd2e8a79f015433f11e4bd393464f0accc2b6
Sthyr <t.lecaros01@gmail.com>
2022-09-09 16:16:28 -0400

    Tutorial React primera parte

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginButton from './components/LoginButton';
+import Profile from './components/Profile';
+import LogoutButton from './components/LogoutButton';
+import {useAuth0} from '@auth0/auth0-react';
+
+
+function App() {
+    const {isAuthenticated} = useAuth0();
+    return (
+    <div className="App">
+      <h1>Application</h1>
+      {
+        isAuthenticated ? <LogoutButton/> : <LoginButton/>
+      }
+      
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
+
