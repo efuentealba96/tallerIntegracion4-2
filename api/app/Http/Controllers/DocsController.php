19e9447378bfce68ebdcdf8a0e9ff03c250b0302
efuentealbaCh <efuentealba.git@gmail.com>
2022-10-25 01:04:32 -0300

    archivos de migracion para crear tablas en base de datos

--- api/app/Http/Controllers/DocsController.php
+++ api/app/Http/Controllers/DocsController.php
@@ -1,6 +1,6 @@
 <?php
 // TODO: creacion de pdf con plantilla html
-namespace App\Http\Controlle1rs;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 //! Es necesario que para la creacion del archivo pdf tengan instlada la libreria DOMPDF
@@ -8,12 +8,14 @@ use Illuminate\Http\Request;
 //? indica que hay que pegar en que archivo y en que seccion (pueden consulatar en caso de dudas)
 
 use Barryvdh\DomPDF\Facade\Pdf;
+use DB;
 class DocsController extends Controller
 {
     public function imprimir(){
     //? en esta seccion se crea el pdf en base a una plantilla
     //? esta plantilla se encuentra en una capeta llamada pdf que esta en viwes
-    $pdf = Pdf::loadView('pdf.plantilla');
+    $user = DB::table('dbo.usuario')->get();
+    $pdf = Pdf::loadView('pdf.plantilla',['usuario'=>$user]);
     return $pdf->stream();
     }
 
